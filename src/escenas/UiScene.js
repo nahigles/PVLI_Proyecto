183ec9064f56b2f59c34551b935c1e28579a8ad5
xtina2434 <crmuno10@ucm.es>
2023-12-20 13:25:50 +0100

    Update UiScene.js

--- src/escenas/UiScene.js
+++ src/escenas/UiScene.js
@@ -104,7 +104,7 @@ export default class UiScene extends Phaser.Scene {
     // Sisema de pausa
     initPauseSystem() {
         // Tecla p
-        this.pauseInput = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+       // this.pauseInput = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
         this.pauseInput.on('down', pointer => { 
             this.pauseGame(true);
         });
